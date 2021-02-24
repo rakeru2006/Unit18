8ba64ed81f1b0175c7fbb0155439558fceb279dd
rakeru2006 <rakeru2006@gmail.com>
2021-02-24 00:13:45 -0600

    Fixing Mongo DB

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
-var express = require("express");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const compression = require('compression');
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static("client"));
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/htmlRoutes.js")(app);
+app.use(express.static('public'));
 
-app.listen(PORT, function() {
-  console.log(`Now listening on port: ${PORT}`);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+// routes
+app.use(require('./routes/api.js'));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
